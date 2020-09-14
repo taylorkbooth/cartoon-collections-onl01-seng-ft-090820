3dcaf0e624dfffd0dc62075b7d439133ea6d3fcd
Taylor Booth <taylorkbooth@gmail.com>
2020-09-14 00:24:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+  planeteer_calls = ["earth", "wind", "fire"]
   planeteer_calls.collect {|x| x + "!" }
   
   
