c7468aeb0eea08fb0d755425062066d23df9ca47
Taylor Booth <taylorkbooth@gmail.com>
2020-09-14 00:30:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ end
 
 def long_planeteer_calls(long_calls)
   long_calls = ["bing", "bang", "boom"]
-  input_array.map {|element| element.length}
+  long_calls.map {|element| element.length}
   
 end
 
