3add90592fe7eaa36a9689b1042285093ad2dba2
Taylor Booth <taylorkbooth@gmail.com>
2020-09-14 00:45:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,6 +24,5 @@ end
 
 def find_the_cheese(cheese_array)
   cheese_array.find do |cheese|
-  
-  cheese_types = ["cheddar", "gouda", "camembert"]
+    cheese_types = ["cheddar", "gouda", "camembert"]
 end
